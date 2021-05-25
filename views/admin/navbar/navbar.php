230cb99d4edf155fdd34fbc444b0436293b21301
José Deodanes-code <60304928+Jose-developer-start@users.noreply.github.com>
2021-05-25 11:05:58 -0600

    Update

--- views/admin/navbar/navbar.php
+++ views/admin/navbar/navbar.php
@@ -8,27 +8,17 @@
       <li class="nav-item active">
         <a class="nav-link" href="index.php" style="color:#ff7043;">Inicio <span class="sr-only">(current)</span></a>
       </li>
-
-      <!-- <li class="nav-item">
-             <a href="" class="btn nav-item productos" style="color:#ff7043;">Productos</a>
-      </li>-->
       <li class="nav-item">
      <li class="nav-item">
              <a href="" class="btn nav-item procesos-v" style="color:#ff7043;">Procesos </a>
       </li>
-
-
-  <!--  <li class="nav-item">
-             <a href="" class="btn nav-item reportes1" style="color:#ff7043;">Reportes</a>
-      </li>-->
-        
-        <!-- form action="#" method="POST">
-          <button class="btn nav-link" name="vista_usuario" href="#">Usuario</button>
-        </form -->
         <a href="" class="btn nav-item usuario" style="color:#ff7043;">Usuario</a>
       </li>
       <li class="nav-item">
         <a class="btn nav-item ventas" href="#" style="color:#ff7043;">Ventas</a>
+      </li>
+      <li class="nav-item">
+        <a class="btn nav-item config-GUI" href="#" style="color:#ff7043;">Configurar GUI</a>
       </li>
     </ul>
     <span class="navbar-text">
